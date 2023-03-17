99c7be14efd1dcc00ec59189ebe84bfe35503632
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-03-17 18:31:02 +0800

    patch: add link to GitHub repo source code

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,4 @@
-import {Button, Container, Divider, Sheet, Typography, useColorScheme} from '@mui/joy'
+import {Button, Container, Divider, Sheet, Stack, Typography, useColorScheme} from '@mui/joy'
 
 const Footer = () => {
   const { mode, setMode } = useColorScheme();
@@ -8,12 +8,20 @@ const Footer = () => {
     <Container sx={{py: 2}}>
       <Typography level={'h6'}>Spending Calculator</Typography>
       <Typography level={'body2'} mb={1.5}>Another demo made with ❤️ by Vincent</Typography>
-      <Button
-        variant={'outlined'} color={'neutral'}
-        size={'sm'}
-        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}>
-        {mode === 'dark' ? 'Turn the lights on' : 'Turn the moon on'}
-      </Button>
+      <Stack direction={'row'} spacing={2}>
+        <Button
+          variant={'outlined'} color={'neutral'} size={'sm'}
+          onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}>
+          {mode === 'dark' ? 'Turn the lights on' : 'Turn the moon on'}
+        </Button>
+        <Button
+          size={'sm'} variant={'outlined'}
+          target={'_blank'}
+          component={'a'} href={'https://github.com/cryptoAlgorithm/react-spending-calculator-demo/'}
+        >
+          View Source on GitHub
+        </Button>
+      </Stack>
     </Container>
   </Sheet>
 }
